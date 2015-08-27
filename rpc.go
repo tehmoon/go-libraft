a62155cb6d707a7af75f031b1b6bef9571cea4df
Edouard Buschini <moon@hathor.intrardc.rdc>
2015-08-27 20:57:53 +0200

    Start now return an Error Server now accept a mux object

--- rpc.go
+++ rpc.go
@@ -2,20 +2,30 @@ package raft
 
 import (
   "net/http"
+  "fmt"
 )
 
 type RPC struct {
   Server *http.Server
 }
 
-func (rpc *RPC) Start() {
-  rpc.Server.ListenAndServe()
+func (rpc *RPC) Start() error {
+  err := rpc.Server.ListenAndServe()
+
+  return err
 }
 
 func NewRPC() *RPC {
+  mux := http.NewServeMux()
+
   server := &http.Server{
-    Addr: ":8080",
+    Addr: ":8081",
+    Handler: mux,
   }
+
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World!")
+  })
 
   rpc := &RPC{
     Server: server,
