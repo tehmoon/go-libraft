cc568d355294fa7e6070c02b0f8bdf4cd66af2b1
Edouard Buschini <moon@hathor.intrardc.rdc>
2015-09-07 18:19:15 +0200

    Starting to work on the HTTP API

--- rpc.go
+++ rpc.go
@@ -3,6 +3,9 @@ package raft
 import (
   "net/http"
   "fmt"
+  "io/ioutil"
+  "strconv"
+  "encoding/json"
 )
 
 // Used to store any field to interact with the RPC
@@ -23,6 +26,7 @@ func (rpc *RPC) Start() error {
 }
 
 // Create RPC connection
+// TODO: create error object for http responses
 func NewRPC(sm *StateMachine) (*RPC, error) {
   mux := http.NewServeMux()
 
@@ -39,11 +43,77 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
   mux.HandleFunc("/appendEntry", func(w http.ResponseWriter, r *http.Request) {
     err := r.ParseForm()
     if err != nil {
-      w.WriteHeader(406)
+      w.WriteHeader(422)
       return
     }
 
-    // TODO: Parse payload
+    // start parsing term
+    term, ok := r.Form["term"]
+    if !ok || term[0] == "" {
+      w.WriteHeader(422)
+      return
+    }
+
+    termId, err := strconv.ParseUint(term[0], 10, 0)
+    if err != nil {
+      w.WriteHeader(422)
+      return
+    }
+
+    if termId < sm.State.CurrentTerm {
+      w.WriteHeader(422)
+      return
+    }
+    // end parsing term
+
+    // start parsing prevLogIndex
+    prevLogIndex, ok := r.Form["prevLogIndex"]
+    if !ok || prevLogIndex[0] == "" {
+      w.WriteHeader(422)
+      return
+    }
+
+    prevIndex, err := strconv.ParseUint(prevLogIndex[0], 10, 0)
+    if err != nil {
+      w.WriteHeader(422)
+      return
+    }
+
+    // if the previous index of the leader is superior to
+    // the number of logs stored on this server then
+    // tell him to retry sending logs but with a lower index
+    if prevIndex > sm.Storage.Index {
+      w.WriteHeader(416)
+      return
+    }
+
+    // end parsing term
+
+    // start parsing body
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+      w.WriteHeader(400)
+      return
+    }
+
+    switch contentType := r.Header.Get("Content-Type"); contentType {
+      case "application/json":
+        var logs Logs
+
+        err := json.Unmarshal(body, &logs)
+        if err != nil {
+          w.WriteHeader(400)
+          return
+        }
+
+        fmt.Println(logs)
+      default:
+        w.WriteHeader(415)
+        return
+    }
+    // end parsing body
+
+
   })
 
   rpc := &RPC{
