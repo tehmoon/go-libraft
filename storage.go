cdd415e0ac502a23397940599fc7dd6dd18fd808
Edouard Buschini <moon@hathor.intrardc.rdc>
2015-09-07 17:31:39 +0200

    Modified Storage to also store the term

--- storage.go
+++ storage.go
@@ -11,28 +11,39 @@ import (
 type Storage struct {
   // Logs are in memory for now but it's going to change
   // when Raft will be implemented
-  Log []interface{}
+  Logs []*Log
 
   // Maintain the index in memory
   Index uint64
 
   // Sync things
-  C chan interface{}
+  C chan struct{}
 
   // The state object the Storage can be reused
   State *State
 }
 
+type Log struct {
+  // The term when the index has been stored
+  Term uint64
+
+  // The actual log
+  Payload interface{}
+
+  // Litteral without specify names wont work
+  _ struct{}
+}
+
 // Append a log to the Logs array.
 // It can be any value
 // Returns the index
-func (s *Storage) AppendLog(payload interface{}) (uint64, error) {
+func (s *Storage) AppendLog(term uint64, payload interface{}) (uint64, error) {
   // Cannot store anything until state has not changed
   if s.State.Is() == INIT {
     return s.Index, fmt.Errorf("System is not ready yet.")
   }
 
-  s.C <- payload
+  s.C <- struct{}{}
 
   // Append the log to memory
   // and set the index to the length
@@ -40,14 +51,18 @@ func (s *Storage) AppendLog(payload interface{}) (uint64, error) {
   // With on-disk array the index will be incremented
   // like s.Index = s.Index + 1 after log has safely
   // been written to storage.
-  s.Log = append(s.Log, payload)
-  s.Index = uint64(len(s.Log))
+  log := &Log{
+    Term: term,
+    Payload: payload,
+  }
 
-  // Simulate the storage, 10 ms correspond to a ssd access
+  s.Logs = append(s.Logs, log)
+  s.Index = s.Index + 1
+
+  // Simulate the storage, 10 ms
   <- time.Tick(10 * time.Millisecond)
 
-  // Apply
-  payload = <- s.C
+  <- s.C
 
   return s.Index, nil
 }
@@ -66,9 +81,9 @@ func (s *Storage) Start() error {
 // to initialize the storage and the index
 func NewStorage(state *State) (*Storage, error) {
   storage := &Storage{
-    Log: make([]interface{}, 0),
+    Logs: make([]*Log, 0),
     Index: 0,
-    C: make(chan interface{}, 1),
+    C: make(chan struct{}, 1),
     State: state,
   }
 
