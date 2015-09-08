135616476ce4e76285fcc12ff65cae11006145d5
Edouard Buschini <moon@hathor.intrardc.rdc>
2015-09-08 17:27:47 +0200

    State lock has moved at the begining of the callback, since we read, we do not want dirty reads Added a response header X-Current-Index to specify the leader at which index the state machine is

--- rpc.go
+++ rpc.go
@@ -41,27 +41,43 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
     fmt.Fprintf(w, "Hello World!")
   })
   mux.HandleFunc("/appendEntry", func(w http.ResponseWriter, r *http.Request) {
+    // Declare a variable that will be used to be the statusCode of the response
+    // because WriteHeader actually writes header, it is not possible to play
+    // with the header in a defer function's call
+    statusCode := 200
+
+    // Acquire the lock since we'll write
+    sm.State.C <- struct{}{}
+    defer func() {
+      // Send back the Current-Term in the response
+      w.Header().Add("X-Current-Term", strconv.FormatUint(sm.State.CurrentTerm, 10))
+      w.WriteHeader(statusCode)
+
+      // Unlock
+      <- sm.State.C
+    }()
+
     err := r.ParseForm()
     if err != nil {
-      w.WriteHeader(422)
+      statusCode = 422
       return
     }
 
     // start parsing term
     term, ok := r.Form["term"]
     if !ok || term[0] == "" {
-      w.WriteHeader(422)
+      statusCode = 422
       return
     }
 
     termId, err := strconv.ParseUint(term[0], 10, 0)
     if err != nil {
-      w.WriteHeader(422)
+      statusCode = 422
       return
     }
 
     if termId < sm.State.CurrentTerm {
-      w.WriteHeader(422)
+      statusCode = 422
       return
     }
     // end parsing term
@@ -69,13 +85,13 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
     //// start parsing leaderCommit
     //leaderCommitValue, ok := r.Form["leaderCommit"]
     //if !ok || leaderCommitValue[0] == "" {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
 
     //leaderCommit, err := strconv.ParseInt(leaderCommitValue[0], 10, 0)
     //if err != nil {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
     //// end parsing leaderCommit
@@ -83,13 +99,13 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
     //// start parsing prevLogTerm
     //prevLogTerm, ok := r.Form["prevLogTerm"]
     //if !ok || prevLogTerm[0] == "" {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
 
     //prevTerm, err := strconv.ParseUint(prevLogTerm[0], 10, 0)
     //if err != nil {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
     //// end parsing prevLogTerm
@@ -97,13 +113,13 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
     //// start parsing prevLogIndex
     //prevLogIndex, ok := r.Form["prevLogIndex"]
     //if !ok || prevLogIndex[0] == "" {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
 
     //prevIndex, err := strconv.ParseInt(prevLogIndex[0], 10, 0)
     //if err != nil {
-      //w.WriteHeader(422)
+      //statusCode = 422
       //return
     //}
 
@@ -118,33 +134,29 @@ func NewRPC(sm *StateMachine) (*RPC, error) {
     // start parsing body
     body, err := ioutil.ReadAll(r.Body)
     if err != nil {
-      w.WriteHeader(400)
+      statusCode = 400
       return
     }
-
     switch contentType := r.Header.Get("Content-Type"); contentType {
       case "application/json":
         var logs Logs
 
         err := json.Unmarshal(body, &logs)
         if err != nil {
-          w.WriteHeader(400)
+          statusCode = 400
           return
         }
 
-        if len(logs) == 0 {
-          sm.State.C <- struct{}{}
+        if len(logs) == 0 && termId != sm.State.CurrentTerm {
           sm.State.CurrentTerm = termId
-          <- sm.State.C
+          sm.Exec("term::changed", termId)
         }
-
-
-        fmt.Println(sm.Storage.Logs)
       default:
-        w.WriteHeader(415)
+        statusCode = 415
         return
     }
     // end parsing body
+
   })
 
   rpc := &RPC{
